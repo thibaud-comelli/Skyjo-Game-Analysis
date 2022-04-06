7900e5d04541e480ebfd30ec9714eafd64d8ced6
thibaud <thibaud.comelli@polytech-lille.net>
2022-04-06 12:22:34 +0200

    Test de git et micro-début

---
+++ Code/untitled0.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 15:08:05 2022
+
+@author: Thibaud
+"""
+
+
+#%%
+
+class Game_instance:
+    
+    nombre_de_joueur = 4
+    joueurs = tableau 8 objet joueur
+    plateau = {
+        "pioche"   : object deck,
+        "defausse" : object carte,
+        "zones"    : tableau 8 object zone, (zone : matrice 4 * 3 object carte)
+        "points"   : [0,0,0,0,0,0,0,0]
+        }
+    
+    
+    def __init__(self):
+        pass
+    
+    def manche:
+        # Préparation d'une manche
+        ## Initialisation de la pioche
+        ## Initialisation des zones
+        ## Initialisation de la défausse
+        
+        
+        pass
+    
+    def partie:
+        pass
+    
+
+
+#%%
