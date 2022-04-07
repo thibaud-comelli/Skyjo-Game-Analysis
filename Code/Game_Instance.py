103db764824e76ac2eae63b0adf474f08db6a84c
thibaud <thibaud.comelli@polytech-lille.net>
2022-04-07 18:56:40 +0200

    petite progression, premières bases

--- Code/Game_Instance.py
+++ Code/Game_Instance.py
@@ -8,14 +8,14 @@ Created on Tue Apr  5 15:08:05 2022
 
 #%%
 
-class Game_instance:
+class Game_Instance:
     
-    nombre_de_joueur = 4
-    joueurs = tableau 8 objet joueur
+    nb_j = 4 à 8 
+    joueurs = tableau 8 object joueur
     plateau = {
         "pioche"   : object deck,
         "defausse" : object carte,
-        "zones"    : tableau 8 object zone, (zone : matrice 4 * 3 object carte)
+        "zones"    : object tableau 8 object zone, (zone : matrice 4 * 3 object carte)
         "points"   : [0,0,0,0,0,0,0,0]
         }
     
@@ -25,9 +25,33 @@ class Game_instance:
     
     def manche:
         # Préparation d'une manche
-        ## Initialisation de la pioche
-        ## Initialisation des zones
-        ## Initialisation de la défausse
+        deck = self.plateau["pioche"]
+        zones = self.plateau["zones"]
+        
+        deck.reset()
+        deck.distribuer(nombre_de_joueur = nb_j,zones)
+        defausse = deck.tirer()
+        
+        joueurs = self.joueurs
+        
+        # Début d'une manche
+        for joueur in joueurs:
+            joueur.retourne_2()
+        
+        j_actif = zones.plus_grand() #si égalité faire un top juste pour compter pour moi
+        
+        # Déroulemet d'une manche
+        while not joueurs[j_actif].fin() : # Fin d'une manche
+            self.jouer(j_actif)
+            j_actif = (j_actif + 1) % nb_j
+        
+        # Décompte des points
+        for joueur in joueurs:
+            joueur.retourne_tout()
+        j_petit = zones.plus_petit() #si égalité perdu
+        
+        
+   
         
         
         pass
